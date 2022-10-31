86a5932095f11046bd2bc7ba48a997190d93a5c3
Yuming Long <63475068+yuming-long@users.noreply.github.com>
2022-10-31 18:13:04 -0400

    chore: use `tempfile` as temporary directory to upload pdf for API (#6)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
-## 0.1.0-dev1
+## 0.1.0-dev2
 
+* Added a temporary directory to upload file to API 
 * Added a /healthcheck endpoint
 * Added an API endpoint for detectron2 layout detection
--- ml_inference/api.py
+++ ml_inference/api.py
@@ -1,21 +1,16 @@
 from fastapi import FastAPI, File, status, Request, UploadFile
 from ml_inference.inference.layout import DocumentLayout
-import os
-import shutil
+import tempfile
 
 app = FastAPI()
 
 
 @app.post("/layout/pdf")
 async def layout_parsing_pdf(file: UploadFile = File()):
-    path = "uploaded_files/"
-    os.mkdir(path)
-    file_location = os.path.join(path, file.filename)
-    with open(file_location, "wb") as f:
-        f.write(file.file.read())
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_file.write(file.file.read())
+        layout = DocumentLayout.from_file(tmp_file.name)
 
-    layout = DocumentLayout.from_file(file_location)
-    shutil.rmtree(path)
     pages_layout = [
         {"number": page.number, "elements": [element.to_dict() for element in page.elements]}
         for page in layout.pages
