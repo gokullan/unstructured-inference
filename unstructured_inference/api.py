c439c83c77ef60f5ba3b0680473fd9b534ed4fda
Benjamin Torres <benjamin@unstructured.io>
2023-01-10 17:34:32 -0600

    feat: added onnx inference base code

--- unstructured_inference/api.py
+++ unstructured_inference/api.py
@@ -3,6 +3,17 @@ from unstructured_inference.inference.layout import DocumentLayout
 from unstructured_inference.models import get_model
 from typing import List
 import tempfile
+import cv2
+import onnxruntime
+from yolox_functions import preproc as preprocess
+from yolox_functions import demo_postprocess,multiclass_nms
+from visualize import vis
+import numpy as np
+import os
+
+LAYOUT_CLASSES=["Caption","Footnote","Formula","List-item","Page-footer","Page-header","Picture","Section-header","Table","Text","Title"]
+YOLOX_MODEL="path/..."
+output_dir="..."
 
 app = FastAPI()
 
@@ -38,6 +49,48 @@ async def layout_parsing_pdf(
     ]
 
     return {"pages": pages_layout}
+
+@app.post("/layout_v0.2/image")
+async def layout_v02_parsing_image(
+    file: UploadFile = File(),
+    include_elems: List[str] = Form(default=ALL_ELEMS),
+    model: str = Form(default=None),
+):
+    # The model was trained and exported with this shape
+    # TODO: check other shapes for inference
+    input_shape = (1024,768) 
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        tmp_file.write(file.file.read())
+        origin_img = cv2.imread(tmp_file)
+        img,ratio = preprocess(origin_img,input_shape)
+
+        session= onnxruntime.InferenceSession(YOLOX_MODEL)
+
+        ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
+        output = session.run(None, ort_inputs)
+        predictions = demo_postprocess(output[0], input_shape, p6=False)[0] #TODO: check for p6
+ 
+        boxes = predictions[:, :4]
+        scores = predictions[:, 4:5] * predictions[:, 5:]
+
+        boxes_xyxy = np.ones_like(boxes)
+        boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2]/2.
+        boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3]/2.
+        boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
+        boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
+        boxes_xyxy /= ratio
+        dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
+        if dets is not None:
+            final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
+            origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
+                            conf=0.3, class_names=LAYOUT_CLASSES)
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, os.path.basename(tmp_file))
+        cv2.imwrite(output_path, origin_img)
+        #Returning dummy value temporarily
+    return {"RESULT": "IMAGE_PROCESSED"}
 
 
 @app.get("/healthcheck", status_code=status.HTTP_200_OK)
